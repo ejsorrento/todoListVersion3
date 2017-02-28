1e1aa0d2e2c8f17303907c070f56ce7c48ea1bcd
EJ Sorrento <ejsorrento@gmail.com>
2017-02-28 14:48:51 -0500

    has addTodos method

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ var todoList = {
     todos: ['item 1', 'item 2', 'itemy 3'],
     displayTodos: function() {
         console.log('My todos', this.todos);
+    },
+    addTodo: function(todo) {
+      this.todos.push(todo);
+      this.displayTodos();
     }
 };
